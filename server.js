610b22f78ac4d7fc6e746f1f28749a9599e04efc
Connor Rowland <cjr6734@yahoo.com>
2015-07-17 12:16:09 -0400

    added node modules

---
+++ server.js
@@ -0,0 +1,29 @@
+var root = __dirname;
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+var methodOverride = require('method-override');
+var logger = require('morgan');
+var path = require('path');
+var db = require('./db.js');
+
+
+app.listen(3000);
+
+app.engine('handlebars', exphbs({defaultLayout: 'main', extname: 'handlebars'}));
+app.set('views', path.join(root, 'views'));
+app.set('view engine', 'handlebars');
+
+app.use(bodyParser.urlencoded());
+app.use(express.static('public'));
+app.use(logger('dev'));
+
+app.use(methodOverride(function(req, res) {
+ if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+   // look in urlencoded POST bodies and delete it
+   var method = req.body._method
+   delete req.body._method
+   return method
+ }
+}));
